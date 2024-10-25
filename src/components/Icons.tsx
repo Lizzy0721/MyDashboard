2681cb90e3dc2ad3737d88e2849645a3552fc6e3
Vio-ES <viony.elizabeth@ui.ac.id>
2024-10-25 22:06:11 +0700

    fix childern error at icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,4 +1,6 @@
-export default function Icons({children}){
+import { ReactNode } from "react";
+
+export default function Icons({ children }:{children: ReactNode}){
     return(
         <button className="rounded-full p-1 outline outline-2 outline-offset-2 outline-dark_moss_green-300 bg-dark_moss_green-200 text-dark_moss_green-500">
             {children}
